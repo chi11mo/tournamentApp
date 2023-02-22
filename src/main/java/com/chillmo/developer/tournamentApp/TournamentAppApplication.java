d49df584446c46c1ebc8e07ff865f953cd74a1b5
Dominic Wenzel <dwenzel@techfak.de>
2023-02-22 13:30:24 +0100

    intialize project

---
+++ src/main/java/com/chillmo/developer/tournamentApp/TournamentAppApplication.java
@@ -0,0 +1,13 @@
+package com.chillmo.developer.tournamentApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TournamentAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TournamentAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chillmo/developer/tournamentApp/TournamentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chillmo.developer.tournamentApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TournamentAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
